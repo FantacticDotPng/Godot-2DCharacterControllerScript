d969d0821b3eac2fec607646495f707212b0aa88
Fantactic <98134369+FantacticDotPng@users.noreply.github.com>
2025-09-14 23:31:29 -0500

    Refactored 2DCharacterController.gd
    The code was a mess before and very hard to work with.
    While the code is still outdated and could be written better, it is at least organized now and it gives you some breathing room.

--- Scripts/2DCharacterController.gd
+++ Scripts/2DCharacterController.gd
@@ -99,10 +99,23 @@ extends Movement2D
 
 #endregion
 
-#region Non-exported variables
+#region Internal variables
+# What direction the player is pressing
+var direction = 0
+
+#region Jumping variables
 # If the player is currently trying to jump
 var JumpInput = false
 
+# If the player is still holding the jump button after a jump has started
+# Variable automatically goes back to false when the player starts falling
+var HoldingJumpInput = false
+
+# Set to true if the player is at the Peak of their jump, used when calculating air momentum
+var PeakOfJump : bool
+#endregion
+
+#region Coyote time variables
 # If the player can activate coyote time, resets when landing
 # This is to prevent the timer from resetting when in the air and not just leaving the ground
 var CanCoyoteTime
@@ -115,16 +128,7 @@ var CurrCoyoteTimer = 0
 
 # The current time on the jump buffer timer
 var CurrJumpBufferTimer = 0
-
-# If the player is still holding the jump button after a jump has started
-# Variable automatically goes back to false when the player starts falling
-var HoldingJumpInput = false
-
-# What direction the player is pressing
-var direction = 0
-
-# Set to true if the player is at the Peak of their jump, used when calculating air momentum
-var PeakOfJump : bool
+#endregion
 
 # Something to do with ray casts, I genuinely do not know
 var space_state
@@ -133,6 +137,15 @@ var space_state
 # player changes size)
 var ColliderSize
 #endregion
+
+# It is recomended you create your own variables in this section.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
+	
+	
+	
+	
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 func _ready():
 	# Gets the size of the player's collider, if your collider changes
@@ -161,9 +174,98 @@ func _physics_process(delta):
 			JumpInput = false
 #endregion
 	
-	#region Grounded/Ungrounded code
+	## It is suggested that you eventually replace this with a proper input
+	## system in your own project
+	#region Input
+	#region Jump Input
+	# Detect if the player is trying to jump and enable the jump buffer timer
+	if Input.is_action_just_pressed("jump"):
+		JumpInput = true
+		CurrJumpBufferTimer = JumpBufferTimerLength
 	
-		#region Grounded code
+	# Detect if the player has let go of the jump button while rising up
+	if Input.is_action_just_released("jump"):
+		if (HoldingJumpInput):
+			CurrentVelocity.y = CurrentVelocity.y / 2
+		HoldingJumpInput = false
+	#endregion
+	#region Horizontal input
+	direction = Input.get_action_strength("right") - Input.get_action_strength("left")
+	# If the player is moving, add .31 to their input strength and then clamp
+	# (this makes it so controlers don't need to hold a direction perfectly to go full speed)
+	if (direction != 0):
+		direction += sign(direction) * .31
+		direction = clamp(direction, -1, 1)
+	#endregion
+	#endregion
+	
+	# re-enable one way platform collision if the player
+	# has let go of the "down" key
+	if(Input.is_action_pressed("down") == false):
+		set_collision_mask_value(2, true)
+	
+	# It is recomended you write your own code in this section.
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
+	
+	
+	
+	
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	# calculates all the fancy movement code, but doesn't actually apply it
+	calculate_movement();
+	
+	# code that nudges the player over if they hit a corner, would not suggest lookinng into since
+	# I don't even know how it works
+	corner_nudging();
+	
+	## play_animations will only run correctly if you have an AnimatedSprite2D on your object
+	## as well as having the correct animations
+	#play_animations();
+	
+	# apply force
+	super._physics_process(delta)
+
+func play_animations():
+	# check to see if the player is grounded
+	if(is_on_floor()):
+		# check to see if the player is moving or not
+		# (we check for 25% of the player's movment speed, aka soft speed cap
+		# because if we were to check for zero then the walking animation
+		# would continue even though the player has pretty much stopped
+		if (abs(CurrentVelocity.x) < SoftSpeedCap * .25):
+			# Play idle animation
+			$AnimatedSprite2D.play("Idle")
+		else:
+			# Play running animation using player movement speed
+			# to determin play speed
+			$AnimatedSprite2D.play("Run", clamp(abs(CurrentVelocity.x / SoftSpeedCap), .2 ,MaxRunningAnimationSpeed))
+	else:
+		# check if the player is going up or not
+		# (reminder that -y is up and positve y is down)
+		if (CurrentVelocity.y < 0):
+			# Play rising animation
+			$AnimatedSprite2D.play("Rise")
+		else:
+			# Play falling animation
+			$AnimatedSprite2D.play("Fall")
+		
+	# Check if the player is moving at all then if they are flip their
+	# sprite accordingly
+	if (CurrentVelocity.x != 0):
+		$AnimatedSprite2D.flip_h = (sign(CurrentVelocity.x) == -1)
+
+func remove_jump():
+	CoyoteTime = false
+	CurrCoyoteTimer = 0
+	HoldingJumpInput = false
+
+## It is not suggested you meass with any of the methods in this region,
+## Unless you know what you're doing
+#region DANGER ZONE
+## Easier to lump together (translation: I got lazy lol, sorry)
+func gravity_and_CoyoteTime_code():
+	#region Grounded code
 	# Detect if player is on the ground to reset the coyote timer and vertical velocity
 	if is_on_floor():
 		# Resets coyote timer
@@ -174,9 +276,8 @@ func _physics_process(delta):
 		FlatGravIncrease = 0
 		# Re-enables platform collision
 		set_collision_mask_value(2, true)
-#endregion
-
-		#region Ungrounded code
+	#endregion
+	#region Ungrounded code
 	else:
 		# Set coyote timer
 		if (CanCoyoteTime):
@@ -196,24 +297,94 @@ func _physics_process(delta):
 				FlatGravIncrease = RisingGravityIncrease
 			else:
 				HoldingJumpInput = false
-#endregion
-#endregion
+	#endregion
 
-	#region Jump Input
-	# Detect if the player is trying to jump and enable the jump buffer timer
-	if Input.is_action_just_pressed("jump"):
-		JumpInput = true
-		CurrJumpBufferTimer = JumpBufferTimerLength
+func calculate_movement():
 	
-	# Detect if the player has let go of the jump button while rising up
-	if Input.is_action_just_released("jump"):
-		if (HoldingJumpInput):
-			CurrentVelocity.y = CurrentVelocity.y / 2
-		HoldingJumpInput = false
-#endregion
+	if is_on_floor():
+		calculate_ground_movement();
+	else:
+		calculate_air_movement();
+	snap_movment();
 
-	#region Jumping
-	# Start jump
+## Snaps the player to a velocity of 0 if they are low enough speed and snaps them to the soft speed
+## Limit if they are near it, to prevent studdering
+func snap_movment():
+	# Apply Hard speed cap as well as setting player velocity to 0 if 
+	if (sign(CurrentVelocity.x) == direction and(abs(CurrentVelocity.x) / SoftSpeedCap) >.75 and (abs(CurrentVelocity.x) / SoftSpeedCap) < 1.1):
+		# If the player is near the soft speed cap then set the player's current velocity equal to the
+		# soft speed cap
+		CurrentVelocity.x = SoftSpeedCap * direction
+	else: if (!apply_hard_speed_cap() and CurrentVelocity.x < 10 and CurrentVelocity.x > -10):
+		# Set player's velocity to 0
+		CurrentVelocity.x = 0
+
+func apply_hard_speed_cap():
+	# Check to see if the player is above the hard speed cap
+	if (abs(CurrentVelocity.x) > HardSpeedCap):
+		# Set the velocity equal to the hard speed cap
+		CurrentVelocity.x = HardSpeedCap * sign(CurrentVelocity.x)
+		# Apply High Speed Deceleration
+		CurrentVelocity.x -= HighSpeedDeceleration * sign(CurrentVelocity.x)
+		return true;
+	return false;
+
+func calculate_ground_movement():
+	# Makes sure that the player is inputing a direction before we apply their input
+	if (abs(CurrentVelocity.x) <= (SoftSpeedCap * abs(direction)) and direction):
+		# Checks if the player is trying to turn around and if the turing acceleration would be more
+		# than the normal acceleration or not
+		if (direction != sign(CurrentVelocity.x) and TuringAcceleration * (abs(CurrentVelocity.x) / SoftSpeedCap) > NormalAcceleration):
+			# Applies Truning Acceleration
+			CurrentVelocity.x += (TuringAcceleration * (abs(CurrentVelocity.x) / SoftSpeedCap)) * direction
+		else:
+			# Cecks if the player is near the soft cap or not
+			if (sign(CurrentVelocity.x) == direction and (abs(CurrentVelocity.x) / SoftSpeedCap) >.75 and (abs(CurrentVelocity.x) / SoftSpeedCap) < 1.1):
+				# If the player is near the soft speed cap then set the player's current velocity equal to the
+				# soft speed cap
+				CurrentVelocity.x = SoftSpeedCap * direction
+			else:
+				# Apply normal acceleration
+				CurrentVelocity.x += NormalAcceleration * direction
+	decelerate();
+
+func calculate_air_movement():
+	# Makes sure that the player is inputing a direction before we apply their input
+	if (abs(CurrentVelocity.x) <= (SoftSpeedCap * abs(direction)) and direction):
+		if(PeakOfJump):
+			# apply 200% of the air acceleration to give more controle at the Peak
+			CurrentVelocity.x += (AirAcceleration * 2) * direction
+		else:
+			# Apply air acceleration
+			CurrentVelocity.x += AirAcceleration * direction
+	decelerate();
+
+func decelerate():
+	# check to see if we are high speed
+	if (abs(CurrentVelocity.x) / SoftSpeedCap >= 1.1):
+		# check to see if the player is holding the opposite direction of where they are moving
+		if (sign(-direction) == sign(CurrentVelocity.x)):
+			# Check to see if the player is grounded or not
+			if(is_on_floor()):
+				# Apply breaking speed
+				CurrentVelocity.x += BreakingSpeed * direction
+			else:
+				# Apply air breaking speed
+				CurrentVelocity.x += AirBreakingSpeed * direction
+		else:
+			# Appy high speed Deceleration if the player is not holding backwards
+			CurrentVelocity.x -= HighSpeedDeceleration * sign(CurrentVelocity.x)
+	else:
+		if (is_on_floor()):
+			print_debug("floor");
+			# Apply normal Deceleration while within soft speed cap
+			CurrentVelocity.x -= (NormalDeceleration * sign(CurrentVelocity.x)) * (abs(CurrentVelocity.x) / SoftSpeedCap)
+		else:
+			print_debug("floorn't");
+			# Apply air Deceleration while within soft speed cap
+			CurrentVelocity.x -= (AirAcceleration * sign(CurrentVelocity.x)) * (abs(CurrentVelocity.x) / SoftSpeedCap)
+
+func jumping():
 	if (JumpInput and (is_on_floor() or CoyoteTime)):
 		# Check if trying to pass through a platform
 		if (is_on_floor() and Input.is_action_pressed("down") and Input.get_action_strength("down") > .5 and get_last_slide_collision() and get_last_slide_collision().get_collider().get_collision_layer() == 2):
@@ -236,108 +407,8 @@ func _physics_process(delta):
 		CoyoteTime = false
 		CanCoyoteTime = false
 		CurrCoyoteTimer = 0
-#endregion
 
-	# re-enable one way platform collision if the player
-	# has let go of the "down" key
-	if(Input.is_action_pressed("down") == false):
-		set_collision_mask_value(2, true)
-
-	#region Horizontal Input
-	# Get player's horizontal input
-	direction = Input.get_action_strength("right") - Input.get_action_strength("left")
-	# If the player is moving, add .31 to their input strength and then clamp
-	# (this makes it so controlers don't need to hold a direction perfectly to go full speed)
-	if (direction != 0):
-		direction += sign(direction) * .31
-		direction = clamp(direction, -1, 1)
-#endregion
-		
-	# Makes sure that the player is both inputing a direction and is not over the soft speed cap
-	# before applying acceleration of decceleration acordingly
-	if (abs(CurrentVelocity.x) <= (SoftSpeedCap * abs(direction)) and direction):
-		if(is_on_floor()):
-			#region Normal Ground Movement
-		# Checks if the player is trying to turn around and if the turing acceleration would be more
-		# than the normal acceleration or not
-			if (direction != sign(CurrentVelocity.x) and TuringAcceleration * (abs(CurrentVelocity.x) / SoftSpeedCap) > NormalAcceleration):
-				# Applies Truning Acceleration
-				CurrentVelocity.x += (TuringAcceleration * (abs(CurrentVelocity.x) / SoftSpeedCap)) * direction
-			else:
-				# Cecks if the player is near the soft cap or not
-				if (sign(CurrentVelocity.x) == direction and (abs(CurrentVelocity.x) / SoftSpeedCap) >.75 and (abs(CurrentVelocity.x) / SoftSpeedCap) < 1.1):
-					# If the player is near the soft speed cap then set the player's current velocity equal to the
-					# soft speed cap
-					CurrentVelocity.x = SoftSpeedCap * direction
-				else:
-					# Apply normal acceleration
-					CurrentVelocity.x += NormalAcceleration * direction
-#endregion
-		else:
-			#region Normal Air Movement
-			# Cecks if the player is near the soft cap or not
-				if (sign(CurrentVelocity.x) == direction and(abs(CurrentVelocity.x) / SoftSpeedCap) >.75 and (abs(CurrentVelocity.x) / SoftSpeedCap) < 1.1):
-					# If the player is near the soft speed cap then set the player's current velocity equal to the
-					# soft speed cap
-					CurrentVelocity.x = SoftSpeedCap * direction
-				else:
-					# Checks if the player is at the Peak of a jump
-					if(PeakOfJump):
-						# apply 200% of the air acceleration to give more controle at the Peak
-						CurrentVelocity.x += (AirAcceleration * 2) * direction
-					else:
-						# Apply air acceleration
-						CurrentVelocity.x += AirAcceleration * direction
-#endregion
-
-	# Branch for if the player is over soft speed cap or is not imputing anything
-	else:
-		# Check to see if the player is close to stopping or not
-		if (CurrentVelocity.x < 10 and CurrentVelocity.x > -10):
-			# Set player's velocity to 0 if they are close to stopping
-			CurrentVelocity.x = 0
-		else:
-			# Check if the player is moving below or slightly above the Soft speed cap
-			if (abs(CurrentVelocity.x) / SoftSpeedCap >= 1.1):
-				# Check and see if the player is over the hard speed cap or not
-				if (abs(CurrentVelocity.x) > HardSpeedCap):
-					#region Prevent player from going over hard speed cap
-					# Set the velocity equal to the hard speed cap
-					CurrentVelocity.x = HardSpeedCap * sign(CurrentVelocity.x)
-					# Apply High Speed Deceleration
-					CurrentVelocity.x -= HighSpeedDeceleration * sign(CurrentVelocity.x)
-#endregion
-				else:
-					#region Calculate high speed deceleration
-					# Check to see if the player is in the air or not
-					if(is_on_floor()):
-						# Apply Sigh Speed deceleration
-						CurrentVelocity.x -= HighSpeedDeceleration * sign(CurrentVelocity.x) 
-					else:
-						# Apply air deceleration
-						CurrentVelocity.x -= AirDeceleration * sign(CurrentVelocity.x)
-					# if the player is holding the opposite direction of their x velocity then
-					# apply the normal movement speed againt their current x velocity
-				# Check to see if the player is holding the opposite direction
-				if (sign(-direction) == sign(CurrentVelocity.x)):
-					# Check to see if the player is grounded or not
-					if(is_on_floor()):
-						# Apply breaking speed
-						CurrentVelocity.x += BreakingSpeed * direction
-					else:
-						# Apply air breaking speed
-						CurrentVelocity.x += AirBreakingSpeed * direction
-#endregion
-			else:
-				# Check to see if the player is in the air or not
-				if (is_on_floor()):
-					# Apply normal Deceleration while within soft speed cap
-					CurrentVelocity.x -= (NormalDeceleration * sign(CurrentVelocity.x)) * (abs(CurrentVelocity.x) / SoftSpeedCap)
-				else:
-					# Apply air Deceleration while within soft speed cap
-					CurrentVelocity.x -= (AirAcceleration * sign(CurrentVelocity.x)) * (abs(CurrentVelocity.x) / SoftSpeedCap)
-			
-	
+func corner_nudging():
 	var EnableCollision = true
 	
 	var Center = transform.origin + $CollisionShape2D.transform.origin
@@ -491,40 +562,4 @@ func _physics_process(delta):
 	if(EnableCollision):
 		# Enables the player's collision again
 		set_collision_mask_value(1, true)
-	
-	super._physics_process(delta)
-	
-#region Animation
-	# check to see if the player is grounded
-	if(is_on_floor()):
-		# check to see if the player is moving or not
-		# (we check for 25% of the player's movment speed, aka soft speed cap
-		# because if we were to check for zero then the walking animation
-		# would continue even though the player has pretty much stopped
-		if (abs(CurrentVelocity.x) < SoftSpeedCap * .25):
-			# Play idle animation
-			$AnimatedSprite2D.play("Idle")
-		else:
-			# Play running animation using player movement speed
-			# to determin play speed
-			$AnimatedSprite2D.play("Run", clamp(abs(CurrentVelocity.x / SoftSpeedCap), .2 ,MaxRunningAnimationSpeed))
-	else:
-		# check if the player is going up or not
-		# (reminder that -y is up and positve y is down)
-		if (CurrentVelocity.y < 0):
-			# Play rising animation
-			$AnimatedSprite2D.play("Rise")
-		else:
-			# Play falling animation
-			$AnimatedSprite2D.play("Fall")
-		
-	# Check if the player is moving at all then if they are flip their
-	# sprite accordingly
-	if (CurrentVelocity.x != 0):
-		$AnimatedSprite2D.flip_h = (sign(CurrentVelocity.x) == -1)
 #endregion
-	
-func remove_jump():
-	CoyoteTime = false
-	CurrCoyoteTimer = 0
-	HoldingJumpInput = false
